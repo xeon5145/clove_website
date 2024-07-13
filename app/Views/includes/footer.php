16fb54dc794c493dfac46229cfc933afac358693
Abhishek Kagra <abhisheksingh5145@gmail.com>
2024-07-13 22:11:06 +0530

    altered view function to add header and footer

---
+++ app/Views/includes/footer.php
@@ -0,0 +1,5 @@
+<div>
+    <h4>Footer</h4>
+</div>
+</body>
+</html>
---
+++ app/Views/includes/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Clove</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= base_url('css/bootstrap.min.css') ?>">
+</head>
+<body>
+    <div>
+        <h4>Header</h4>
+    </div>
