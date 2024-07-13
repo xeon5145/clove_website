69faa75d0f961f23dfa0b419223ab17603c11a10
Abhishek Kagra <abhisheksingh5145@gmail.com>
2024-07-13 22:25:37 +0530

    updated structure

--- app/Views/includes/header.php
+++ app/Views/includes/header.php
@@ -1,12 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Clove</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?= base_url('css/bootstrap.min.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('css/main.css') ?>">
 </head>
+
 <body>
-    <div>
-        <h4>Header</h4>
+    <div class="row p-2">
+        <!-- Link logo -->
+        <div class="col-1">
+            <a class="" href="#"><img class="clove-logo" src="<?= base_url('assets/logo/clove_logo.png') ?>" /></a>
+        </div>
+        <!-- Link logo -->
+
     </div>
---
+++ app/Views/main.php
@@ -0,0 +1,11 @@
+<!-- page - 1 -->
+<div></div>
+<!-- page - 1 -->
+
+<!-- page - 2 -->
+<div></div>
+<!-- page - 2 -->
+ 
+<!-- page - 3 -->
+<div></div>
+<!-- page - 3 -->
