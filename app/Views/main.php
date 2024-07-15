f6605648e55c0e663fd1e56f6f51f96272f00716
Abhishek Kagra <abhisheksingh5145@gmail.com>
2024-07-15 21:48:14 +0530

    added content on page-1

--- app/Views/main.php
+++ app/Views/main.php
@@ -1,11 +1,41 @@
 <!-- page - 1 -->
-<div></div>
+<div class="grd-primary page-1">
+    <div class="row mt-3 mb-3">
+        <div class="col-6">
+            <div class="row ms-5 mt-5 mb-5">
+                <div class="col">
+                    <h3 class="text-main fw-bold">Clove is the next gen payment system operated by network</h3>
+
+                    <div class="row mt-3">
+                        <div class="col bg-main p-5 clove-card">
+                            <h4 class="text-secondary fw-bold">What is Clove</h4>
+
+                            <p class="text-secondary">
+                                Clove operates using peer-to-peer technology, eliminating the need for a central authority or banks.
+                                Transactions and the issuance of Cloves are managed collectively by the network.
+                                <b>
+                                    As an open-source project,
+                                    Clove's design is publicly available, ensuring that no single entity owns or controls it,
+                                    and anyone can participate.
+                                </b>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <!-- page - 1 -->
 
 <!-- page - 2 -->
-<div></div>
+<div>
+    <h2>Chain Simulation</h2>
+</div>
 <!-- page - 2 -->
- 
+
 <!-- page - 3 -->
-<div></div>
+<div class="bg-main">
+    <h2>Market Chart</h2>
+</div>
 <!-- page - 3 -->
